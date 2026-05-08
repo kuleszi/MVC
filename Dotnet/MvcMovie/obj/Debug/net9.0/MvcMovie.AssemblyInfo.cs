f05ad9e6458f3edf499ede06c29415ac28424ca3
kuleszi <aleksandrakulesza.96@gmail.com>
2026-05-08 22:55:19 +0200

    [LAB_4] commit for part 9

--- Dotnet/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ Dotnet/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1e4195ef50a212a338e3efcc8942a06fba23f7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef461267d2e8f18c7915c787906744283080ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
